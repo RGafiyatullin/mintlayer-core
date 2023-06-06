6cbba8b421dd27f8fbc555e60b41134b03a2f34a
Lukas Kuklinek <lukas.kuklinek@mintlayer.org>
2023-06-06 16:43:36 +0100

    mem usage

--- mempool/src/pool/store/mem_usage.rs
+++ mempool/src/pool/store/mem_usage.rs
@@ -15,44 +15,84 @@
 
 //! Estimate and track memory usage taken by data structures.
 
-use std::mem;
+use std::{
+    mem,
+    sync::atomic::{AtomicUsize, Ordering},
+};
 
 use common::chain::{
     signature::inputsig::InputWitness, stakelock::StakePoolData, SignedTransaction, TxInput,
     TxOutput,
 };
 
+use super::TxMempoolEntry;
+
 /// Structure that stores the current memory usage and keeps track of its changes
+#[derive(Debug)]
 pub struct Tracker {
-    current_usage: usize,
+    current_usage: AtomicUsize,
 }
 
 impl Tracker {
     pub fn new() -> Self {
-        Self { current_usage: 0 }
+        let current_usage = AtomicUsize::new(0);
+        Self { current_usage }
     }
 
     pub fn get_usage(&self) -> usize {
-        self.current_usage
+        self.current_usage.load(Ordering::Acquire)
+    }
+
+    fn add(&self, amount: usize) {
+        self.current_usage.fetch_add(amount, Ordering::AcqRel);
+    }
+
+    fn sub(&self, amount: usize) {
+        self.current_usage.fetch_sub(amount, Ordering::AcqRel);
     }
 }
 
 /// A data structure which has its memory consumption tracked
 #[must_use = "Memory-tracked object dropped without using Tracked::release"]
-#[derive(Eq, PartialEq, PartialOrd, Ord)]
-pub struct Tracked<T>(T);
+#[derive(Eq, PartialEq, PartialOrd, Ord, Debug)]
+pub struct Tracked<T, D> {
+    obj: T,
+    drop_policy: D,
+}
 
-impl<T: MemUsage> Tracked<T> {
+impl<T: ZeroUsageDefault, D: Default> Default for Tracked<T, D> {
+    fn default() -> Self {
+        let obj = T::default();
+        assert_eq!(
+            obj.indirect_memory_usage(),
+            0,
+            "Default not zero-size despite being marked as such"
+        );
+
+        let drop_policy = D::default();
+        Self { obj, drop_policy }
+    }
+}
+
+impl<T: MemoryUsage, D: DropPolicy + Default> Tracked<T, D> {
     /// Create a new object with tracked memory usage
-    pub fn new(tracker: &mut Tracker, obj: T) -> Self {
-        tracker.current_usage += obj.indirect_memory_usage();
-        Self(obj)
+    pub fn new(tracker: &Tracker, obj: T) -> Self {
+        tracker.add(obj.indirect_memory_usage());
+        let drop_policy = D::default();
+        Self { obj, drop_policy }
     }
 
     /// Release the object from the tracker and return it as a value
-    pub fn release(this: Self, tracker: &mut Tracker) -> T {
-        tracker.current_usage -= this.0.indirect_memory_usage();
-        this.0
+    pub fn release(this: Self, tracker: &Tracker) -> T {
+        tracker.sub(this.obj.indirect_memory_usage());
+        Self::forget(this)
+    }
+
+    /// Forget about the object being tracked without updating the tracker. Useful during tear down
+    /// when the tracker is no longer in use.
+    pub fn forget(mut this: Self) -> T {
+        this.drop_policy.on_release();
+        this.obj
     }
 
     /// Get mutable access to the tracked object.
@@ -60,43 +100,92 @@ impl<T: MemUsage> Tracked<T> {
     /// Memory usage update is done automatically by the [Guard] object upon when it falls out of
     /// scope. Since this exclusively borrows the tracker, the scope of the resulting guard should
     /// be minimized as much as possible.
-    pub fn get_mut<'t>(&mut self, tracker: &'t mut Tracker) -> Guard<'_, 't, T> {
-        Guard::new(&mut self.0, tracker)
+    pub fn get_mut<'t>(&mut self, tracker: &'t Tracker) -> Guard<'_, 't, T> {
+        Guard::new(&mut self.obj, tracker)
     }
 }
 
-impl<T> std::ops::Deref for Tracked<T> {
+impl<T, D> std::ops::Deref for Tracked<T, D> {
     type Target = T;
 
     fn deref(&self) -> &T {
-        &self.0
+        &self.obj
     }
 }
 
-/// Guard that mutably borrows and object and updates the mem tracker once modifications are done.
-pub struct Guard<'a, 't, T: MemUsage> {
-    tracker: &'t mut Tracker,
-    object: &'a mut T,
+/// What to do with a [Tracked] object if it's dropped without being released. The actual handling
+/// of the drop logic is done in the policy type's Drop trait.
+pub trait DropPolicy {
+    fn on_release(&mut self) {}
 }
 
-impl<'a, 't, T: MemUsage> Guard<'a, 't, T> {
-    fn new(object: &'a mut T, tracker: &'t mut Tracker) -> Self {
-        // Deduct the usage of this object. After the modifications have been done, the new size is
-        // going to be added in the Guard destructor. Since the tracker is borrowed mutably for the
-        // lifetime of the guard, the tracker cannot be queried while the object is being modified.
-        tracker.current_usage -= object.indirect_memory_usage();
+/// Trivial drop policy that does nothing
+#[derive(Eq, PartialEq, PartialOrd, Ord, Debug, Default)]
+pub struct NoopDropPolicy;
 
-        Self { tracker, object }
+impl DropPolicy for NoopDropPolicy {}
+
+/// Drop policy that asserts if the object has not been properly released
+#[derive(Eq, PartialEq, PartialOrd, Ord, Debug)]
+pub struct AssertDropPolicy {
+    released: bool,
+}
+
+impl Default for AssertDropPolicy {
+    fn default() -> Self {
+        Self { released: false }
     }
 }
 
-impl<'a, 't, T: MemUsage> Drop for Guard<'a, 't, T> {
+impl DropPolicy for AssertDropPolicy {
+    fn on_release(&mut self) {
+        self.released = true;
+    }
+}
+
+impl Drop for AssertDropPolicy {
     fn drop(&mut self) {
-        self.tracker.current_usage += self.object.indirect_memory_usage();
+        if !std::thread::panicking() {
+            assert!(
+                self.released,
+                "A memory-tracked value dropped without being released"
+            );
+        }
     }
 }
 
-impl<'a, 't, T: MemUsage> std::ops::Deref for Guard<'a, 't, T> {
+/// Guard that mutably borrows an object and updates the tracker once modifications are done.
+pub struct Guard<'a, 't, T: MemoryUsage> {
+    tracker: &'t Tracker,
+    object: &'a mut T,
+    orig_usage: usize,
+}
+
+impl<'a, 't, T: MemoryUsage> Guard<'a, 't, T> {
+    fn new(object: &'a mut T, tracker: &'t Tracker) -> Self {
+        // Store the original object usage. After all the modifications have been performed,
+        // the new size is going to be recorded in the Guard destructor.
+        Self {
+            tracker,
+            orig_usage: object.indirect_memory_usage(),
+            object,
+        }
+    }
+}
+
+impl<'a, 't, T: MemoryUsage> Drop for Guard<'a, 't, T> {
+    fn drop(&mut self) {
+        let cur_usage = self.object.indirect_memory_usage();
+        let orig_usage = self.orig_usage;
+        match cur_usage.cmp(&orig_usage) {
+            std::cmp::Ordering::Equal => (),
+            std::cmp::Ordering::Less => self.tracker.sub(orig_usage - cur_usage),
+            std::cmp::Ordering::Greater => self.tracker.add(cur_usage - orig_usage),
+        }
+    }
+}
+
+impl<'a, 't, T: MemoryUsage> std::ops::Deref for Guard<'a, 't, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
@@ -104,7 +193,7 @@ impl<'a, 't, T: MemUsage> std::ops::Deref for Guard<'a, 't, T> {
     }
 }
 
-impl<'a, 't, T: MemUsage> std::ops::DerefMut for Guard<'a, 't, T> {
+impl<'a, 't, T: MemoryUsage> std::ops::DerefMut for Guard<'a, 't, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut *self.object
     }
@@ -112,17 +201,15 @@ impl<'a, 't, T: MemUsage> std::ops::DerefMut for Guard<'a, 't, T> {
 
 // Code to estimate size taken up by [std::collections::BTreeSet] or [std::collections::BTreeMap].
 mod btree {
-    use std::{marker::PhantomData, mem};
-
     // The following structs are laid out in the same way as the real standard library equivalents
     // to give a reasonably precise estimation of their sizes. It is possible that the library
     // implementations change in the future. In that case, the estimation becomes less precise
     // although hopefully will remain good enough for our purposes until the structs below are
     // updated to reflect the change. It's still just an estimate after all.
 
-    const B: usize = 6;
-    const CAP: usize = B * 2 - 1;
+    const B: usize = 6; // the B parameter for the B-tree
     const BF: usize = 2 * B; // branching factor
+    const CAP: usize = BF - 1; // data capacity per node
 
     struct _LeafNode<K, V> {
         _parent: *mut (),
@@ -137,18 +224,17 @@ mod btree {
         _children: [*mut (); BF],
     }
 
-    pub struct Tree<K, V>(PhantomData<fn() -> (K, V)>);
-
     /// Estimate the memory usage of the B-tree structure.
     ///
     /// This includes the space taken up by the keys and values stored in the tree. It does NOT
+    /// include data pointed to by keys and values indirectly (e.g. via `Box` or `Vec`).
     pub fn usage<K, V>(num_elems: usize) -> usize {
         // Use u64 internally to avoid possible overflow issues on 32-bit platforms
         let num_elems = num_elems as u64;
 
         // Size of B-tree nodes:
-        let leaf_size = mem::size_of::<_LeafNode<K, V>>() as u64;
-        let internal_size = mem::size_of::<_InternalNode<K, V>>() as u64;
+        let leaf_size = std::mem::size_of::<_LeafNode<K, V>>() as u64;
+        let internal_size = std::mem::size_of::<_InternalNode<K, V>>() as u64;
 
         // Size of all leaf elements.
         let leaves = (leaf_size * num_elems) / CAP as u64;
@@ -157,8 +243,8 @@ mod btree {
         let elems_per_internal_node = (CAP * BF) as u64;
         let internals = (internal_size * num_elems * 11) / (elems_per_internal_node * 10);
 
-        // Total size of the B-tree structure. Assuming nodes are on average 75% full, an
-        // additional overhead is added for the unused occupied space.
+        // Total size of the B-tree structure. Assuming nodes are on average 75% full,
+        // an additional overhead is added for the unused occupied space.
         let total = 4 * (leaves + internals) / 3;
 
         total as usize
@@ -168,7 +254,7 @@ mod btree {
 /// Trait for data types capable of reporting their current memory usage
 ///
 /// TODO: Make this a derivable trait so the `impl`s react automatically to changes.
-pub trait MemUsage {
+pub trait MemoryUsage {
     /// Get amount of memory taken by the data owned by `self` (e.g. if it contains `Box` or `Vec`)
     fn indirect_memory_usage(&self) -> usize;
 
@@ -180,13 +266,13 @@ pub trait MemUsage {
     }
 }
 
-impl MemUsage for u8 {
+impl MemoryUsage for u8 {
     fn indirect_memory_usage(&self) -> usize {
         0
     }
 }
 
-impl<K, V> MemUsage for std::collections::BTreeMap<K, V> {
+impl<K, V> MemoryUsage for std::collections::BTreeMap<K, V> {
     /// The mem usage for [BTreeMap].
     ///
     /// Includes the nodes and the key and value data stored in the nodes. It does not, however,
@@ -198,32 +284,44 @@ impl<K, V> MemUsage for std::collections::BTreeMap<K, V> {
     }
 }
 
-impl<K> MemUsage for std::collections::BTreeSet<K> {
+impl<K> MemoryUsage for std::collections::BTreeSet<K> {
     /// Same limitation as for `BTreeMap` also applies here
     fn indirect_memory_usage(&self) -> usize {
         btree::usage::<K, ()>(self.len())
     }
 }
 
-impl<T: MemUsage> MemUsage for Option<T> {
+impl<T: MemoryUsage> MemoryUsage for Option<T> {
     fn indirect_memory_usage(&self) -> usize {
         self.as_ref().map_or(0, |x| x.indirect_memory_usage())
     }
 }
 
-impl<T: MemUsage> MemUsage for [T] {
+impl<T: MemoryUsage> MemoryUsage for &[T] {
     fn indirect_memory_usage(&self) -> usize {
         self.iter().map(T::indirect_memory_usage).sum::<usize>() + self.len() * mem::size_of::<T>()
     }
 }
 
-impl<T: MemUsage> MemUsage for Vec<T> {
+impl<T: MemoryUsage> MemoryUsage for Vec<T> {
     fn indirect_memory_usage(&self) -> usize {
         self.as_slice().indirect_memory_usage()
     }
 }
 
-impl MemUsage for SignedTransaction {
+impl<T: MemoryUsage> MemoryUsage for Box<T> {
+    fn indirect_memory_usage(&self) -> usize {
+        T::total_memory_usage(self.as_ref())
+    }
+}
+
+impl MemoryUsage for TxMempoolEntry {
+    fn indirect_memory_usage(&self) -> usize {
+        self.transaction().indirect_memory_usage()
+    }
+}
+
+impl MemoryUsage for SignedTransaction {
     /// Only data included indirectly (via pointers). The actual object usage is already contained
     /// in the B-tree map usage.
     fn indirect_memory_usage(&self) -> usize {
@@ -234,20 +332,20 @@ impl MemUsage for SignedTransaction {
     }
 }
 
-impl MemUsage for TxInput {
+impl MemoryUsage for TxInput {
     fn indirect_memory_usage(&self) -> usize {
         // No data owned by this object
         0
     }
 }
 
-impl MemUsage for TxOutput {
+impl MemoryUsage for TxOutput {
     fn indirect_memory_usage(&self) -> usize {
         match self {
             TxOutput::Transfer(_, _) => 0,
             TxOutput::LockThenTransfer(_, _, _) => 0,
             TxOutput::Burn(_) => 0,
-            TxOutput::CreateStakePool(_, _) => mem::size_of::<StakePoolData>(),
+            TxOutput::CreateStakePool(_, pd) => pd.indirect_memory_usage(),
             TxOutput::ProduceBlockFromStake(_, _) => 0,
             TxOutput::CreateDelegationId(_, _) => 0,
             TxOutput::DelegateStaking(_, _) => 0,
@@ -255,7 +353,13 @@ impl MemUsage for TxOutput {
     }
 }
 
-impl MemUsage for InputWitness {
+impl MemoryUsage for StakePoolData {
+    fn indirect_memory_usage(&self) -> usize {
+        0
+    }
+}
+
+impl MemoryUsage for InputWitness {
     fn indirect_memory_usage(&self) -> usize {
         match self {
             InputWitness::NoSignature(data) => data.indirect_memory_usage(),
@@ -263,3 +367,9 @@ impl MemUsage for InputWitness {
         }
     }
 }
+
+/// Types where the object created by T::default() takes no indirect memory.
+pub trait ZeroUsageDefault: MemoryUsage + Default {}
+
+impl<K, V> ZeroUsageDefault for std::collections::BTreeMap<K, V> {}
+impl<K> ZeroUsageDefault for std::collections::BTreeSet<K> {}
